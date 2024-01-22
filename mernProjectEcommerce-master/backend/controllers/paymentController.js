4de7203ec6c7e0cc2740ee071bc2b22b3ca7cbf2
Anubhav404 <84443881+Anubhav404@users.noreply.github.com>
2024-01-23 00:44:29 +0530

    Update paymentController.js

--- mernProjectEcommerce-master/backend/controllers/paymentController.js
+++ mernProjectEcommerce-master/backend/controllers/paymentController.js
@@ -1,21 +1,4 @@
-const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+/// payment controller code 
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-  const myPayment = await stripe.paymentIntents.create({
-    amount: req.body.amount,
-    currency: "inr",
-    metadata: {
-      company: "Ecommerce",
-    },
-  });
-
-  res
-    .status(200)
-    .json({ success: true, client_secret: myPayment.client_secret });
-});
-
-exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
-});
+//using stripe payment
+//stripe ki site se
